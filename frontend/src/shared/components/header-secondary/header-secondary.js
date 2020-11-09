b67f5240e5a9df2a5f2b05bfacc953c70628fd2e
ARTAS <artasss47@gmail.com>
2020-11-09 19:58:50 +0100

    deleted unneeded icons

--- frontend/src/shared/components/header-secondary/header-secondary.js
+++ frontend/src/shared/components/header-secondary/header-secondary.js
@@ -15,42 +15,6 @@ const HeaderSecondary = () => {
       }}
     >
       <Searchbar style={{ justifyContent: "center" }} />
-      <div
-        style={{
-          position: "absolute",
-          right: "10rem",
-        }}
-      >
-        <AppsIcon style={{ color: "#fff", width: "4rem", height: "4rem" }} />
-        <ViewCarouselIcon
-          style={{
-            color: "#fff",
-            width: "4rem",
-            height: "4rem",
-            marginLeft: "2rem",
-          }}
-        />
-        {/* <FormControl variant="outlined">
-          <InputLabel id="demo-simple-select-filled-label">Age</InputLabel>
-          <Select
-            autoWidth={false}
-            style={{ backgroundColor: "#fff", width: "15rem" }}
-            variant="outlined"
-            color="red"
-            labelId="demo-simple-select-filled-label"
-            id="demo-simple-select-filled"
-            // value={age}
-            onChange={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl> */}
-      </div>
     </div>
   );
 };
