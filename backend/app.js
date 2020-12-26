83064593253946e9879d85c95caa501e21aa193f
Artas47 <artasss47@gmail.com>
2020-12-27 00:52:36 +0100

    changed middleware order

--- backend/app.js
+++ backend/app.js
@@ -28,19 +28,19 @@ app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(express.json());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-  next();
-});
-
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    
+    next();
+  });
+  
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
